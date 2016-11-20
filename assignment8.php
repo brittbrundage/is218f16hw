11c4f9204a0d70f182beee05166e05dd94aa57b4
Brittani <bmb23@njit.edu>
2016-11-20 16:55:08 -0500

    construct

--- assignment8.php
+++ assignment8.php
@@ -16,6 +16,26 @@ echo $string->__toString();
 
 echo "<br><br>";
 
+//ReflectionProperty Construct
+
+class dog {
+    public $x = 1;
+    protected $y = 2;
+    private $z = 3;
+}
+
+$obj = new dog;
+
+$prop = new ReflectionProperty('dog', 'y');
+$prop->setAccessible(true); 
+var_dump($prop->getValue($obj)); 
+
+$prop = new ReflectionProperty('dog', 'z');
+$prop->setAccessible(true); 
+var_dump($prop->getValue($obj)); 
+
+
+echo "<br><br>";
 
 
 
