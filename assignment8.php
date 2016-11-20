2ae2cf7cb4259d7fb49597fa48d7d0e3aa2c6d6e
Brittani <bmb23@njit.edu>
2016-11-20 16:27:23 -0500

    getextension

--- assignment8.php
+++ assignment8.php
@@ -1,5 +1,14 @@
 <?php
-echo "Assignment 8";
+echo "Working with Objects and References<br><br>";
+
+//ReflectionClass getExtension 
+
+$class = new ReflectionClass('ReflectionClass');
+$extension = $class->getExtension();
+var_dump($extension);
+
+echo "<br><br>";
+
 
 
 ?>
