89d8a051f9fd94a23af7407f65b43bdb14d47952
Brittani <bmb23@njit.edu>
2016-11-20 20:20:21 -0500

    file not found // file opened`

--- assignment9.php
+++ assignment9.php
@@ -2,5 +2,16 @@
 
 echo "Exception Handling";
 
+echo "<br><br>";
+
+if(!file_exists("abc.csv")) {
+      die("File not found");
+   }
+   
+else {
+      $file = fopen("abc.csv","r");
+      print "File Opened sucessfully";
+   }
+
 
 ?>
