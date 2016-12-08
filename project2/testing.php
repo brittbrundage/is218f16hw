744253c7e26279a4959f7117463822db9c3553ae
Brittani <bmb23@njit.edu>
2016-12-08 13:32:36 -0500

    starting to fill in testing page

--- project2/testing.php
+++ project2/testing.php
@@ -1,7 +1,19 @@
+<html>
+<head>
+</head>
+<body>
 <?php
+include('bootstrap.php');
+echo 'Brittani Brundage Project 2';
 
-echo"Developing";
+//Unordered List
+
+echo 'Cars';
+$cars = array('Toyota Supra','Ford Mustang GT', 'Dodge Challengar Hellcat', 'Mitshubushi Eclipse GSX');
+$bmb->UnorderedList($cars);
 
 
 
 ?>
+</body>
+</html>
