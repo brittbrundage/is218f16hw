c6f38abd0e3427f7a5835e31a16927b374533467
Brittani <bmb23@njit.edu>
2016-12-19 21:07:52 -0500

    HTTP GET request

--- homework12.php
+++ homework12.php
@@ -2,15 +2,17 @@
 
 echo "<b><big>Homework 12 PHP Curl Requests</big><b><br>";
 
-echo "Sending HTTP Get Request with Curl";
+echo "Sending HTTP Get Request with Curl<br><br>";
 
 $ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,"http://google.com");
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
 curl_setopt($ch,CURLOPT_HEADER,true);
 
 $output = curl_exec($ch);
 curl_close($ch);
 
-echo "Sending HTTP Post Request with Curl";
+echo "<br><br>Sending HTTP Post Request with Curl<br><br>";
 
 
 
