6e21d556dddb04242d69c4a9a312b325e4b30c40
Brittani <bmb23@njit.edu>
2016-12-19 20:58:06 -0500

    Sending HTTP Get Request with Curl

--- homework12.php
+++ homework12.php
@@ -2,6 +2,16 @@
 
 echo "<b><big>Homework 12 PHP Curl Requests</big><b><br>";
 
+echo "Sending HTTP Get Request with Curl";
+
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_HEADER,true);
+
+$output = curl_exec($ch);
+curl_close($ch);
+
+echo "Sending HTTP Post Request with Curl";
+
 
 
 
