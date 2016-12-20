d281f9c6785e06191706706fac01868b096fa5c0
Brittani <bmb23@njit.edu>
2016-12-20 16:12:20 -0500

    HTTP Post

--- homework12.php
+++ homework12.php
@@ -29,7 +29,33 @@ function httpGet($url)
  
 echo httpGet("http://google.com");
 
+echo "<br><br>";
 
+echo "<br><br>Sending HTTP POST Request with Curl<br><br>";
+
+function httpPost($url,$params)
+{
+  $postData = '';
+   foreach($params as $k => $v) 
+   { 
+      $postData .= $k . '='.$v.'&'; 
+   }
+   $postData = rtrim($postData, '&');
+ 
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($ch,CURLOPT_HEADER, false); 
+    curl_setopt($ch, CURLOPT_POST, count($postData));
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+ 
+}
 
 
 ?>
