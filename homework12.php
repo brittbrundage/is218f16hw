142fdc72df901cec4dc3cfe8679d83c4f046a47b
Brittani <bmb23@njit.edu>
2016-12-19 21:15:45 -0500

    fixed http get

--- homework12.php
+++ homework12.php
@@ -1,18 +1,33 @@
 <?php
 
-echo "<b><big>Homework 12 PHP Curl Requests</big><b><br>";
+echo "<b><big>Homework 12 PHP Curl Requests</big></b><br>";
 
-echo "Sending HTTP Get Request with Curl<br><br>";
 
-$ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,"http://google.com");
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_HEADER,true);
 
-$output = curl_exec($ch);
-curl_close($ch);
+//$ch = curl_init();
+//curl_setopt($ch,CURLOPT_URL,"http://google.com");
+//curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+//curl_setopt($ch,CURLOPT_HEADER,true);
 
-echo "<br><br>Sending HTTP Post Request with Curl<br><br>";
+//$output = curl_exec($ch);
+//curl_close($ch);
+
+echo "<br><br>Sending HTTP GET Request with Curl<br><br>";
+
+function httpGet($url)
+{
+    $ch = curl_init();  
+ 
+    curl_setopt($ch,CURLOPT_URL,$url);
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+ 
+    $output=curl_exec($ch);
+ 
+    curl_close($ch);
+    return $output;
+}
+ 
+echo httpGet("http://google.com");
 
 
 
