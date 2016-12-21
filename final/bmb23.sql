3988d74ee3d289db52fc9c20129fdeb7f0dd4989
Brittani <bmb23@njit.edu>
2016-12-20 19:44:33 -0500

    sql

--- final/bmb23.sql
+++ final/bmb23.sql
@@ -1,9 +1,9 @@
-CREATE TABLE IF NOT EXISTS `members` (
-  `memberID` int(11) NOT NULL AUTO_INCREMENT,
-  `member_username` varchar(255) NOT NULL,
-  `member_password` varchar(255) NOT NULL,
-  `member_email` varchar(255) NOT NULL,
-  PRIMARY KEY (`memberID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
+CREATE DATABASE `final` ;
+CREATE TABLE `final`.`users` (
+   `user_id` INT( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+   `user_name` VARCHAR( 255 ) NOT NULL ,
+   `user_email` VARCHAR( 60 ) NOT NULL ,
+   `user_pass` VARCHAR( 255 ) NOT NULL ,
+    UNIQUE (`user_name`),
+    UNIQUE (`user_email`)
+) ENGINE = MYISAM ;
