8e70425c01ad26aca7ba831e5f61cee691862f2b
Brittani <bmb23@njit.edu>
2016-12-21 13:22:09 -0500

    updated welcome dashboard

--- final/welcome.php
+++ final/welcome.php
@@ -16,11 +16,17 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
 <link rel="stylesheet" href="style.css" type="text/css"  />
 
+
+
+
+
 <ul class="topnav" id="myTopnav">
-  <li><a  href="https://web.njit.edu/~bmb23/is218f16/final/index.php">Home</a></li>
+  <li><a  href="https://web.njit.edu/~bmb23/is218f16/final/home.php">Home</a></li>
   <li><a class="active" href="https://web.njit.edu/~bmb23/is218f16/final/welcome.php">My Account</a></li>
-  <li><a href="logout.php"><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
-    <
+  <li><a  href="https://web.njit.edu/~bmb23/is218f16/final/update.php?user_id=<?php echo $userRow['user_id'];?>">Edit Account</a></li>
+   <li><a  href="https://web.njit.edu/~bmb23/is218f16/final/users.php">Registered Users</a></li>
+  <li><a href="logout.php"> Logout</a></li>
+    
 </ul>
 
 <title>Welcome: <?php print($userRow['user_name']); ?></title>
