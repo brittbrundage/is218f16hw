93c0fe9ee17a6f9bbc694b577be0bedb004eda07
Brittani <bmb23@njit.edu>
2016-12-20 23:44:09 -0500

    changed index page, added sign in page

--- final/index.php
+++ final/index.php
@@ -1,67 +1,15 @@
-<?php
-require_once 'dbconfig.php';
+<html>
 
-if($user->is_loggedin()!="")
-{
- $user->redirect('home.php');
-}
+<ul class="topnav" id="myTopnav">
+  <li><a class="active" href="https://web.njit.edu/~bmb23/is218f16/final/index.php">Home</a></li>
+  <li><a href="https://web.njit.edu/~bmb23/is218f16/final/signin.php">Log In</a></li>
+  <li><a href="https://web.njit.edu/~bmb23/is218f16/final/signup.php">Sign Up</a></li>
+</ul>
 
-if(isset($_POST['btn-login']))
-{
- $uname = $_POST['txt_uname_email'];
- $umail = $_POST['txt_uname_email'];
- $upass = $_POST['txt_password'];
-  
- if($user->login($uname,$umail,$upass))
- {
-  $user->redirect('home.php');
- }
- else
- {
-  $error = "Try Again!";
- } 
-}
-?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Login</title>
-<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
-<link rel="stylesheet" href="style.css" type="text/css"  />
-</head>
+
+
 <body>
-<div class="container">
-     <div class="form-container">
-        <form method="post">
-            <h2>Sign in</h2><hr />
-            <?php
-            if(isset($error))
-            {
-                  ?>
-                  <div class="alert alert-danger">
-                      <i class="glyphicon glyphicon-warning-sign"></i> &nbsp; <?php echo $error; ?> !
-                  </div>
-                  <?php
-            }
-            ?>
-            <div class="form-group">
-             <input type="text" class="form-control" name="txt_uname_email" placeholder="Username or Email ID" required />
-            </div>
-            <div class="form-group">
-             <input type="password" class="form-control" name="txt_password" placeholder="Your Password" required />
-            </div>
-            <div class="clearfix"></div><hr />
-            <div class="form-group">
-             <button type="submit" name="btn-login" class="btn btn-block btn-primary">
-                 <i class="glyphicon glyphicon-log-in"></i>&nbsp;Sign in
-                </button>
-            </div>
-            <br />
-            <label>No account? <a href="signup.php">Sign Up</a></label>
-        </form>
-       </div>
-</div>
+<body bgcolor="#00BFFF">
 
 </body>
 </html>
