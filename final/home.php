ad576acd356c09d890251a858866bf7013127b83
Brittani <bmb23@njit.edu>
2016-12-21 09:17:36 -0500

    welcome page

--- final/home.php
+++ final/home.php
@@ -15,21 +15,28 @@ $userRow=$stmt->fetch(PDO::FETCH_ASSOC);
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css"  />
 <link rel="stylesheet" href="style.css" type="text/css"  />
-<title>welcome - <?php print($userRow['user_email']); ?></title>
+
+<ul class="topnav" id="myTopnav">
+  <li><a class="active" href="https://web.njit.edu/~bmb23/is218f16/final/index.php">Home</a></li>
+  <li><a href="https://web.njit.edu/~bmb23/is218f16/final/signin.php">Log In</a></li>
+  <li><a href="https://web.njit.edu/~bmb23/is218f16/final/signup.php">Sign Up</a></li>
+  <div class="right">
+  <li><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> Logout</a></li>
+    </div>
+</ul>
+
+<title>Welcome: <?php print($userRow['user_name']); ?></title>
+
+
+
+
 </head>
 
 <body>
 
-<div class="header">
- <div class="left">
-     <label><a href="http://www.codingcage.com/">Coding Cage - Programming Blog</a></label>
-    </div>
-    <div class="right">
-     <label><a href="logout.php?logout=true"><i class="glyphicon glyphicon-log-out"></i> logout</a></label>
-    </div>
-</div>
+
 <div class="content">
-welcome : <?php print($userRow['user_name']); ?>
+Welcome: <?php print($userRow['user_name']); ?>
 </div>
 </body>
 </html>
