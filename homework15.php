de60dd9a4013d482b919db0bab5dfbea97ff4e1d
Brittani <bmb23@njit.edu>
2016-12-22 13:33:59 -0500

    view

--- homework15.php
+++ homework15.php
@@ -12,4 +12,20 @@ class Model
     }
 }
 
+class View
+{
+    private $model;
+    private $controller;
+
+    public function __construct($controller,$model) {
+        $this->controller = $controller;
+        $this->model = $model;
+    }
+	
+    public function output(){
+        return "<p>" . $this->model->string . "</p>";
+    }
+}
+
+
 ?>
