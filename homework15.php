8d690c08d7293c4b9698b50051bc8d42bfcae1e5
Brittani <bmb23@njit.edu>
2016-12-22 14:08:11 -0500

    rerouting and urls

--- homework15.php
+++ homework15.php
@@ -50,4 +50,32 @@ if (isset($_GET['action']) && !empty($_GET['action'])) {
 
 echo $view->output();
 
+
+$page = $_GET['page'];
+if (!empty($page)) {
+
+    $data = array(
+        'about' => array('model' => 'AboutModel', 'view' => 'AboutView', 'controller' => 'AboutController'),
+        'portfolio' => array('model' => 'PortfolioModel', 'view' => 'PortfolioView', 'controller' => 'PortfolioController')
+    );
+
+    foreach($data as $key => $components){
+        if ($page == $key) {
+            $model = $components['model'];
+            $view = $components['view'];
+            $controller = $components['controller'];
+            break;
+        }
+    }
+
+    if (isset($model)) {
+        $m = new $model();
+        $c = new $controller($model);
+        $v = new $view($model);
+        echo $v->output();
+    }
+}
+
+
+
 ?>
