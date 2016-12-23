4ce1510942e4dcf62d46cc8fb2dab510282a50af
Brittani <bmb23@njit.edu>
2016-12-22 22:47:55 -0500

    url view class

--- homework15.php
+++ homework15.php
@@ -98,4 +98,21 @@ class Model2
     }
 }
 
+class View2
+{
+    private $model;
+
+    public function __construct($model) {
+        $this->controller = $controller;
+        $this->model = $model;
+    }
+
+    public function output(){
+        $data = "<p>" . $this->model->tstring ."</p>";
+        require_once($this->model->template);
+    }
+}
+
+
+
 ?>
